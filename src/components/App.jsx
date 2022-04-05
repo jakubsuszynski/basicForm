5428917c2592d61ec891fc2985affa32e025a06d
Jakub Suszynski <jakubsuszynski@carfax.com>
2022-04-05 15:14:44 +0200

    Fix docs link responsiveness

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <div className={styles.app}>
             <main className="container d-flex align-items-center flex-column">
                 <Form/>
-                <div className={"col-6 mt-2 d-flex justify-content-end"}>
+                <div className={"col-12 col-md-6 mt-4 d-flex justify-content-end"}>
                     <a href={REPO_URL} rel="noreferrer" target={"_blank"}>{STATIC_TEXTS.DOCS}</a>
                 </div>
             </main>
